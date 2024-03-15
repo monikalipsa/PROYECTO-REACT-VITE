365d009ba96e9e608b18983bc9c6f87cc5010363
monikalipsa <mgp9483@gmail.com>
2024-03-14 22:10:25 -0300

    finalizados forms

--- src/components/formulario/Registro.jsx
+++ src/components/formulario/Registro.jsx
@@ -1,6 +1,6 @@
-//  import React, { useState } from 'react';
+
 //  import Boton from '../../components/boton/Boton';
-//  import './Registro.css';
+  import './Registro.css';
 
  
 
@@ -88,33 +88,46 @@
 //  }
 
 //  export default Registro;
+
+import Boton from '../boton/Boton';
 import React, { useState } from 'react';
 
-function FormularioContacto() {
+function Registro() {
     const [mensaje, setMensaje] = useState('');
     const [camposObligatoriosError, setCamposObligatoriosError] = useState('');
+    const [errores, setErrores] = useState({
+        nombre: '',
+        apellido: '',
+        email: '',
+        telefono: '',
+        usuario: '',
+        clave: '',
+        repetirClave: ''
+    });
 
-    const validarCampo = (elemento) => {
-        const campo = elemento.value.trim();
-        const error = document.getElementById('error_' + elemento.id);
-
-        if (!campo) {
-            error.innerText = 'Este campo es obligatorio';
-            elemento.classList.add('error');
+    const validarCampo = (campo, valor) => {
+        if (!valor.trim()) {
+            setErrores(prevState => ({
+                ...prevState,
+                [campo]: 'Este campo es obligatorio'
+            }));
             return false;
         } else {
-            error.innerText = '';
-            elemento.classList.remove('error');
+            setErrores(prevState => ({
+                ...prevState,
+                [campo]: ''
+            }));
             return true;
         }
     };
 
     const validarFormulario = () => {
-        const camposObligatorios = ['nombre', 'apellido', 'email', 'telefono'];
+        const camposObligatorios = ['nombre', 'apellido', 'email', 'telefono', 'usuario', 'clave', 'repetirClave'];
         let camposIncompletos = false;
 
         camposObligatorios.forEach(campo => {
-            if (!validarCampo(document.getElementById(campo))) {
+            const valor = document.getElementById(campo).value;
+            if (!validarCampo(campo, valor)) {
                 camposIncompletos = true;
             }
         });
@@ -127,10 +140,7 @@ function FormularioContacto() {
     };
 
     const enviarFormulario = () => {
-        // Aquí iría tu lógica de enviar el formulario
-        // Por ejemplo, utilizando fetch o axios para enviar los datos a un servidor
-        // Y luego mostrar el mensaje de éxito o error recibido del servidor
-        setMensaje('Mensaje enviado correctamente');
+        setMensaje('Registro correcto');
         resetearFormulario();
     };
 
@@ -138,60 +148,57 @@ function FormularioContacto() {
         document.getElementById('form_contacto').reset();
     };
 
+     const handlerClicBoton = () => {
+         alert('INGRESANDO');}
+    
     return (
         <section className="formulario">
-            <form id="form_contacto">
+              <h3>REGISTRATE</h3>
+            <form >
                 <div>
                     <label htmlFor="nombre">Nombre : </label>
-                    <input type="text" id="nombre" name="nombre" className="input-field" />
-                    <div id="error_nombre" className="error-message"></div>
+                    <input type="text" id="nombre" name="nombre" className="input-field" onBlur={(e) => validarCampo(e.target.name, e.target.value)} />
+                    <div className="error-message">{errores.nombre}</div>
                 </div>
                 <div>
                     <label htmlFor="apellido">Apellido : </label>
-                    <input type="text" id="apellido" name="apellido" className="input-field" />
-                    <div id="error_apellido" className="error-message"></div>
+                    <input type="text" id="apellido" name="apellido" className="input-field" onBlur={(e) => validarCampo(e.target.name, e.target.value)} />
+                    <div className="error-message">{errores.apellido}</div>
                 </div>
                 <div>
                     <label htmlFor="telefono">Teléfono :</label>
                     <input type="tel" id="telefono" name="telefono" className="input-field" />
                 </div>
+                <br />
                 <div>
                     <label htmlFor="email">Email :</label>
-                    <input type="text" id="email" name="email" className="input-field" />
-                    <div id="error_email" className="error-message"></div>
+                    <input type="text" id="email" name="email" className="input-field" onBlur={(e) => validarCampo(e.target.name, e.target.value)} />
+                    <div className="error-message">{errores.email}</div>
                 </div>
                 <div>
-                    <label htmlFor="tipoContacto">Sexo :</label>
-                    <select id="tipoContacto" name="tipoContacto" className="input-field">
-                        <option value="soporte">Femenino</option>
-                        <option value="ventas">Masculino</option>
-                    </select>
-                    <div id="error_tipoContacto" className="error-message"></div>
-                </div>
-                
-                <div>
-                    <label htmlFor="apellido">Usuario: </label>
-                    <input type="text" id="usuario" name="usuario" className="input-field" />
-                    <div id="error_usuario" className="error-message"></div>
+                    <label htmlFor="usuario">Usuario :</label>
+                    <input type="text" id="usuario" name="usuario" className="input-field" onBlur={(e) => validarCampo(e.target.name, e.target.value)} />
+                    <div className="error-message">{errores.usuario}</div>
                 </div>
                 <div>
-                    <label htmlFor="apellido">Clave : </label>
-                    <input type="text" id="clave" name="clave" className="input-field" />
-                    <div id="error_clave" className="error-message"></div>
-                </div>  <div>
-                    <label htmlFor="apellido">Repetir Clave : </label>
-                    <input type="text" id="repetirClave" name="repetirClave" className="input-field" />
-                    <div id="error_repetirClave" className="error-message"></div>
+                    <label htmlFor="clave">Clave :</label>
+                    <input type="password" id="clave" name="clave" className="input-field" onBlur={(e) => validarCampo(e.target.name, e.target.value)} />
+                    <div className="error-message">{errores.clave}</div>
                 </div>
-                
-                
+                <div>
+                    <label htmlFor="repetirClave">Repetir Clave :</label>
+                    <input type="password" id="repetirClave" name="repetirClave" className="input-field" onBlur={(e) => validarCampo(e.target.name, e.target.value)} />
+                    <div className="error-message">{errores.repetirClave}</div>
+                </div>
                 <div id="camposObligatorios" className="error-message">{camposObligatoriosError}</div>
-                <button type="button" id="enviarBtn" className="submit-button" onClick={() => { setCamposObligatoriosError(''); validarFormulario(); }}>Enviar</button>
-                <div id="mensaje" className="mensaje">{mensaje}</div>
+
+                 <button type="button" id="enviarBtn" className="submit-button" onClick={() => { setCamposObligatoriosError(''); validarFormulario(); }}>Registrarme</button> 
+
+                {/* <div id="mensaje" className="mensaje">{mensaje}</div> */}
             </form>
             <br />
         </section>
     );
 }
 
-export default FormularioContacto;
+export default Registro;
